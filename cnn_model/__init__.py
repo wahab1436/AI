2173f08588bbdce77bcbb7bf16de14b1d96801ef
Abdul Wahab <abdul00wahab1000@gmail.com>
2026-04-20 23:19:24 +0500

    Create __init__.py

---
+++ cnn_model/__init__.py
@@ -0,0 +1,5 @@
+from .model import CNNFeatureExtractor
+from .train import CNNTrainer
+from .extract import EmbeddingExtractor
+
+__all__ = ["CNNFeatureExtractor", "CNNTrainer", "EmbeddingExtractor"]
